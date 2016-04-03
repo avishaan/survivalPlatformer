b8f5def7a52fed7f6c705115253600e4360236ac
Avishaan <avishaan@codeHatcher.com>
2016-04-03 18:08:23 -0500

    add ability to move and rotate character

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,62 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask; // tell raycast only to hit floor
+    float camRayLength = 100f;
+
+    // get's called regardless of whether script is enabled
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // physics related updating
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // do movement
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        // normalize vector
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            // change vector to a rotation/Quaternion
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation); // uses new rotation
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f; // if either h/v not zero, they are walking
+        anim.SetBool("IsWalking", walking); // let animator know we are walking
+    }
 }
